359910f1f9aca4b88bfd20c8fa0bda0ddba05471
Steve Samuels <98560043+SpartnSam@users.noreply.github.com>
2022-03-04 14:42:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "password"
 
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+
 # Test
 def add(a, b):
     return a + b
 
-@app.route("/hello")
+class unprotectedForm(FlaskForm):
+    class Meta:
+        csrf = True # Compliant
+
+@app.route("/hello/")
 def index():
     flash("What is your name?")
     return render_template("index.html")
 
-@app.route("/greet", methods=["POST", "GET"])
+@app.route("/greet/", methods=["POST", "GET"])
 def greet(): 
     flash("Hey " + str(request.form['name_input']) + ", glad to see you here!")
     return render_template("index.html")
+
