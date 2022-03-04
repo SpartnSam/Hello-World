01e6c9f3abaf07f8df0a78d6d37d0cdf8d9a6027
Steve Samuels <98560043+SpartnSam@users.noreply.github.com>
2022-03-04 14:44:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.secret_key = "password"
-
 csrf = CSRFProtect()
 csrf.init_app(app) # Compliant
+app.secret_key = "password"
 
 # Test
 def add(a, b):
